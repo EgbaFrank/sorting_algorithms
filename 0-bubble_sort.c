94b2a09ae8bd506d147c2887692b0306c0b77638
Egba Frank <Egbafrank72@gmail.com>
2024-04-28 20:36:41 +0000

    Added update, -task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,8 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tmp, swapped;
+	if (size < 2)
+		return;
 
 	for (i = 0; i < (size - 1); i++)
 	{
@@ -17,11 +19,11 @@ void bubble_sort(int *array, size_t size)
 
 		for (j = 0; j < (size - i - 1); j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				swapped = 1;
 				print_array(array, size);
 			}
